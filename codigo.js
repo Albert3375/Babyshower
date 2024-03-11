1a49f072ad888b48615607e7e208f7730d052b4c
Alberto Mendez <143474965+Albert3375@users.noreply.github.com>
2024-03-10 22:48:11 -0600

    nuevo

---
+++ codigo.js
@@ -0,0 +1,8 @@
+let nombre = prompt("Como te llamas ");
+
+alert("Bienvenido " + nombre)
+
+var num = parseInt(prompt("Entrar numero entero"));
+var res = num%2;
+var par = res==0;
+alert("¿Es par? " + par);
